8db7eb19510dc019c7b2aabb4fbbd2ce238506f2
Fran Bosquet <fran@redradix.com>
2019-02-01 16:11:10 +0100

    use multiple delimeters

--- bla.test.js
+++ bla.test.js
@@ -8,7 +8,25 @@ describe('add function', () => {
   it('returns number for one number input', () => {
     expect(add('1')).toBe(1)
   })
+
   it('returns sum of numbers for two numbers input', () => {
     expect(add('1,2')).toBe(3)
+  })
+
+  it('return the sum of any bumber of inputs', () => {
+    const testCases = [
+      { input: '4,2,8', output: 14 },
+      { input: '4,2,8,10,1', output: 25 },
+      { input: '4,2,8,10,1,3,3,-4', output: 27 }
+    ]
+
+    testCases.forEach( ({ input, output}) => {
+      expect(add(input)).toBe(output)
+    })
+  })
+
+  it('allows to separate number with line breaks', () => {
+    expect(add('1\n2')).toBe(3)
+    expect(add('1\n2,3')).toBe(6)
   })
 })
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 module.exports = input => {
   if (input.length === 0) return 0
-  const numbers = getNumbers(input)
+  const numbers = parseMultiple(getNumbers(input))
   return sumMultiple(numbers)
 }
 
-const getNumbers = input => input.split(',')
-const sumMultiple = numbers => numbers.reduce(sum, 0)
-const sum = (a, b) => a + parseInt(b)
+const getNumbers = input => input.split(/[,\n]/)
+const parse = n => parseInt(n)
+const parseMultiple = arr => arr.map(parse)
+const sum = (a, b) => a + b
+const sumMultiple = numbers => numbers.reduce(sum)
