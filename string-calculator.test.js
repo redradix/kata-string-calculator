2944705a4b8a5f1d006566a98b2d813103ff2bb3
Ivan Alcalde <ivan@redradix.com>
2022-01-28 16:14:47 +0100

    [REFACTOR] tests - building tests dinamically

--- string-calculator.test.js
+++ string-calculator.test.js
@@ -1,28 +1,25 @@
+const mkTest = (title, input, expectMethod, expected) => ({
+  title,
+  input,
+  expectMethod,
+  expected,
+})
+
+const tests = [
+  mkTest('Adding emtpy string the outcome must be zero', '', 'toBe', 0),
+  mkTest('Adding "1" string the outcome must be 1', '1', 'toBe', 1),
+  mkTest('Adding "1,2,3,4,5" string the outcome must be 15', '1,2,3,4,5', 'toBe', 15),
+  mkTest('Adding "1\n2,3" string the outcome must be 6', '1\n2,3', 'toBe', 6)
+]
+
 const add = require('./string-calculator')
 
 describe('string-calculator', () => {
-  it('Adding emtpy string the outcome must be zero', () => {
-    const result = add('')
-
-    expect(result).toBe(0)
-  })
-
-  it('Adding "1" string the outcome must be 1', () => {
-    const result = add("1")
-
-    expect(result).toBe(1)
-  })
-
-  it('Adding "1,2,3,4,5" string the outcome must be 15', () => {
-    const result = add("1,2,3,4,5")
-
-    expect(result).toBe(15)
-  })
-
-  it('Adding "1\n2,3" string the outcome must be 6', () => {
-    const result = add("1\n2,3")
-
-    expect(result).toBe(6)
+  tests.forEach(({ title, input, expectMethod, expected }) => {
+    it(title, () => {
+      const result = add(input)
+      expect(result)[expectMethod](expected)
+    })
   })
 
   it('Adding "1,\\n" string the outcome must give Exception', () => {
