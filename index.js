d690b0897083b82df0b0709b4364d96675a54717
Juan Redradix <juanportillo@redradix.com>
2022-01-28 12:40:26 +0100

    [green] - Test returned value with single parameter

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const add = (numbers) => {
-  return 0;
+const add = (stringNumbers) => {
+  if (stringNumbers.length === 0) {
+    return 0;
+  }
+  return 1;
 };
 
 module.exports = { add };
--- index.test.js
+++ index.test.js
@@ -3,7 +3,10 @@ const { add } = require("./index.js");
 describe("add", () => {
   it("returns 0 when empty argument is passed", () => {
     const result = add("");
-
     expect(result).toBe(0);
+  });
+  it("returns '1' when single '1' argument is passed", () => {
+    const result = add("1");
+    expect(result).toBe(1);
   });
 });
